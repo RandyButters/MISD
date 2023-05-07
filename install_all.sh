5d681d790dd5336f035904dc691f3316eadb5cd1
RandyButters <105148937+RandyButters@users.noreply.github.com>
2023-05-07 11:20:51 +0800

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,8 +1,8 @@
-conda install pytorch=1.4 cudatoolkit=10.1 -c pytorch
-pip install tensorboard==2.7.0
+conda install pytorch=1.8.1 cudatoolkit=10.1 -c pytorch
+pip install tensorboard==2.10.1
 conda install pandas
 pip install gym
-pip install mujoco_py==2.0.2.8
+pip install mujoco_py==2.1.2.14
 pip install opencv-python
 pip install tqdm
 conda install matplotlib
