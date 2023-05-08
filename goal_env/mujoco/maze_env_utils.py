c6dca1e29ab1393cb5f0e35387950abbce275de4
RandyButters <105148937+RandyButters@users.noreply.github.com>
2023-05-08 09:46:57 +0800

    Update maze_env_utils.py

--- goal_env/mujoco/maze_env_utils.py
+++ goal_env/mujoco/maze_env_utils.py
@@ -110,20 +110,6 @@ def construct_maze(maze_id='Maze'):
             [1, O, O, O, O, O, O, O, O, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ]
-    elif maze_id == 'MazeS':
-        O = 'r'
-        structure = [
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, O, O, O, O, O, O, O, O, 1],
-            [1, O, O, O, O, O, O, O, O, 1],
-            [1, 1, 1, 1, 1, 1, 1, O, O, 1],
-            [1, O, O, O, O, O, O, O, O, 1],
-            [1, O, O, O, O, O, O, O, O, 1],
-            [1, O, O, 1, 1, 1, 1, 1, 1, 1],
-            [1, O, O, O, O, O, O, O, 0, 1],
-            [1, O, O, O, O, O, O, O, 0, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        ]
     elif maze_id == 'MazeB':
         O = 'r'
         structure = [
